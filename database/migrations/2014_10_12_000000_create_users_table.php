92341a6daa9431d9e8d80b3b33eecd32fa2b4259
Ruben <ruben_ventura@outlook.es>
2020-10-11 21:26:06 +0200

    Especialidades

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('dni');            
+            $table->string('adress')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('rol'); // 'admin', 'patient', 'doctor'
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,18 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Speciality
+
+Route::get('/specialties', 'SpecialtyController@index'); // Devuelve una vista con el listado de especialidades
+Route::get('/specialties/create', 'SpecialtyController@create'); //Vista con el formulario de refistro
+Route::get('/specialties/{specialty}/edit', 'SpecialtyController@edit');// Devuelve un formulario de edición con los datos de la especialidad seleccionada
+Route::post('/specialties', 'SpecialtyController@store'); // Envío del formulario de registro
+Route::put('/specialties/{specialty}', 'SpecialtyController@update');// Actualiza los datos de la especialidad
+Route::delete('/specialties/{specialty}', 'SpecialtyController@destroy');// Elimina especialidad
+
+//Doctores
+Route::resource('doctors', 'DoctorController');
+
+//Pacientes
+Route::resource('patients', 'DoctorController');
